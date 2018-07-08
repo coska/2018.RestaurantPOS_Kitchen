bef7341d22f54ba4120910f41f7aa235b5bf128a
test1 <test@test.com>
2018-07-08 01:40:22 -0400

    1)conform eslint sytle, 2)add bind, 3)separate clock

--- screens/HomeScreen/NavigationBar.js
+++ screens/HomeScreen/NavigationBar.js
@@ -2,83 +2,36 @@ import React, { Component } from 'react'
 import {
   Text,
   View,
+  TouchableOpacity,
 } from 'react-native'
 
-import { TouchableOpacity } from 'react-native'
+import KitClock from './KitClock'
 
 import {
   KitNavButtonSty,
   KitNavTextSty,
   KitNavPrevNextSty,
   KitNavPrevNextTextSty,
-  KitNavClockSty,
   KitNavLogoSty,
-
 } from './Styles'
-
 
 //
 // button
 //
-const KitNavButton = ({ children, onPress, style, styleText=KitNavTextSty() }) => (
+const KitNavButton = ({text, onPress, style, styleText = KitNavTextSty()}) => (
   <TouchableOpacity onPress={onPress} style={style}>
     <Text style={styleText}>
-      {children}
+      {text}
     </Text>
   </TouchableOpacity>
 )
-
-//
-// clock
-//
-class KitClock extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {date: this.getDateTime() };
-  }
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000  /* every 1 sec */
-      /*59000 */ /* every 59 sec */
-    );
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({ date: this.getDateTime(), });
-  }
-
-  getDateTime(){
-    cdate = new Date()
-    fdate = (cdate.getMonth() + 1) + ' / '
-          + cdate.getDate() + ' / '
-          + cdate.getFullYear()
-
-    ftime = cdate.getHours() + ':'
-          + cdate.getMinutes() + ':'
-          + cdate.getSeconds()
-
-    return fdate + " " + ftime
-  }
-
-  render() {
-    return ((
-      <Text style={KitNavClockSty()}>
-        {this.state.date}
-      </Text>
-    ));
-  }
-}
 
 //
 // nav bar
 //
 class NavigationBar extends Component {
   state = { }
-  /**
+  /*
   componentDidMount() {
     setInterval( () => {
       this.setState({
@@ -86,73 +39,74 @@ class NavigationBar extends Component {
       })
     },1000)
   }
-  **/
+  */
   render() {
     return (
-    <View style={{ flexDirection: 'row', backgroundColor: 'skyblue', padding:3 }}>
+      <View style={{ flexDirection: 'row', backgroundColor: 'skyblue', padding: 3 }}>
 
-        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-around', borderWidth:0 }}>
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-around' }}>
 
-          {/** LOGO + PREV/NEXT **/}
+          {/* LOGO + PREV/NEXT */}
           <View style={{ flex: 2, borderWidth: 0, flexDirection: 'row'}}>
 
-            <View style={{ flex: 1}}>
-              <Text style={ KitNavLogoSty() }>COSKA SUSHI</Text>
+            <View style={{ flex: 1 }}>
+              <Text style={KitNavLogoSty()}>COSKA SUSHI</Text>
             </View>
 
             <View style={{ flex: 1, borderWidth: 0, flexDirection: 'row' }}>
               <View>
                 <KitNavButton
-                  children ="PREV" /* TODO */
-                  style = { KitNavPrevNextSty() }
-                  styleText = { KitNavPrevNextTextSty() }
+                  text="PREV"
+                  style={KitNavPrevNextSty()}
+                  styleText={KitNavPrevNextTextSty()}
                 />
               </View>
               <View>
                 <KitNavButton
-                  children ="NEXT" /* TODO */
-                  style = { KitNavPrevNextSty() }
-                  styleText = { KitNavPrevNextTextSty() }
+                  text="PREV"
+                  style={KitNavPrevNextSty()}
+                  styleText={KitNavPrevNextTextSty()}
                 />
               </View>
             </View>
-
           </View>
-          {/** CLOCK **/}
-          <View style={{ flex: 1, borderWidth: 0, flexDirection:'row', justifyContent: 'flex-end'}}>
+
+          {/* CLOCK */}
+          <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end' }}>
             <KitClock />
           </View>
+
         </View>
 
         <View style={{ flex: 1, flexDirection: 'row' }}>
           <View>
             <KitNavButton
-              children ="NEW ORDER(#)" /* TODO */
-              style =  { KitNavButtonSty('#EEE') }
+              text="NEW ORDER(#)"
+              style={KitNavButtonSty('#0EE')}
             />
           </View>
           <View>
             <KitNavButton
-              children ="COOKING(#)" /* TODO */
-              style =  { KitNavButtonSty('#EEE') }
+              text="COOKING(#)"
+              style={KitNavButtonSty('#099')}
             />
           </View>
           <View>
             <KitNavButton
-              children ="DONE(#)" /* TODO */
-              style =  { KitNavButtonSty() }
+              text="DONE(#)"
+              style={KitNavButtonSty('#077')}
             />
           </View>
           <View>
             <KitNavButton
-              children ="DAY TOTAL(#)" /* TODO */
-              style =  { KitNavButtonSty() }
+              text="DAY TOTAL(#)"
+              style={KitNavButtonSty('#055')}
             />
           </View>
         </View>
 
 
-    </View>
+      </View>
     )
   }
 }
